89be02e43e52d44a4841b9f14fec46d0c4df1543
Andresjl389 <andresjaimes195@gmail.com>
2023-09-16 15:10:21 -0500

    New endpoint

--- main.py
+++ main.py
@@ -2,34 +2,54 @@ import os
 import mysql.connector 
 from functools import wraps
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mytoken"
 
+try:
+    client = MongoClient("mongodb+srv://Andreses:qO0eRncJlMS4f78t@cluster0.eydrwxt.mongodb.net/")
+    db = client["Generador_Recetas"]
+    collection = db["recetas"]
+    client.server_info()
+    print("Conexión exitosa a la base de datos")
+except Exception as e:
+    print("Error al conectar la base de datos: ", str(e))
+
+# db = client["Generador_Recetas"]
+# collection = db["recetas"]
+# client.server_info()
+# print("conexion exitosa")
+
+
 
 def recipes(Ingredientes_principal = ""):
-    mydb = mysql.connector.connect(
-        host="containers-us-west-164.railway.app",
-        user="root",
-        password="mrBmEjYDry39p8X6mB1a",
-        database="railway",
-        port=6384
-    )
-    mycursor = mydb.cursor()
-    sql = "select * from recipes where Ingrediente_principal = '"+Ingredientes_principal+"'"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    
+    # db = mysql.connector.connect(
+    #     host="andresarevalo.mysql.pythonanywhere-services.com",
+    #     user="andresarevalo",
+    #     password="febrero23",
+    #     database="andresarevalo$Products"
+    # ) 
+    
+    # cursor = db.cursor()
+    # sql = "select * from recipes where Ingrediente_principal = '"+Ingredientes_principal+"'"
+    # cursor.execute(sql)
+    # myresult = mycursor.fetchall()
     return myresult
 
-recetas = recipes()
 
 # Ruta para obtener una receta aleatoriaz
 @app.route('/receta_aleatoria', methods=['GET'])
 def obtener_receta_aleatoria():
-    receta_aleatoria = recipes()
-    print(receta_aleatoria)
-    return jsonify({'receta seleccionada': receta_aleatoria})
+    resultado = collection.find({"ingrediente_principal": "dcsdds"})
+    
+    # receta_aleatoria = recipes()
+    # print(receta_aleatoria)
+    
+    print(busqueda)
+    return jsonify({'receta seleccionada': busqueda})
 
 if __name__ == '_main_':
     app.run(debug=True)
@@ -56,13 +76,15 @@ def receta_Post():
 @app.route('/create_receipe', methods=['POST'])
 
 def create_Post():
-    mydb2 = mysql.connector.connect(
-        host="containers-us-west-164.railway.app",
-        user="root",
-        password="mrBmEjYDry39p8X6mB1a",
-        database="railway",
-        port=6384
-    )
+    
+    # db = mysql.connector.connect(
+    #     host="AndresJaimes389.mysql.pythonanywhere-services.com",
+    #     user="AndresJaimes389",
+    #     password="Gerecetas",
+    #     database="AndresJaimes389$Proyect"
+    # ) 
+    
+    # cursor = db.cursor()
 
     Id = request.form['id']
     NomReceta = request.form['Nombre_receta']
@@ -71,13 +93,10 @@ def create_Post():
     Ingre_Principal = request.form['Ingrediente_principal']
 
 
-   
-
-
-    mycursor = mydb2.cursor()
-    mycursor.execute("INSERT INTO recipes (id, Nombre_receta, Descripcion, Ingredientes, Ingrediente_principal) values (%s, %s, %s, %s, %s)", (Id, NomReceta, Description, Ingredients, Ingre_Principal))   
-
-    mydb2.commit()
+    
+    # cursor.execute("INSERT INTO Recipes (id, nombre, descripcion, ingredientes, ingrediente_principal) values (%s, %s, %s, %s, %s)", (Id, NomReceta, Description, Ingredients, Ingre_Principal))   
+    # db.commit()
+    # cursor.close()
     return jsonify({"Id ": Id, "Nombre receta": NomReceta, "Descripcion ": Description, "Ingredientes ": Ingredients, "Ingrediente principal ": Ingre_Principal})
     
 
@@ -99,9 +118,9 @@ def token_required(f):
 }
 
 
-"""
-MAIN ...........................................................................
-"""
-if __name__ == '__main__':
-    # app.run()
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+# """
+# MAIN ...........................................................................
+# """
+# if __name__ == '__main__':
+#     # app.run()
+#     app.run(debug=True, port=os.getenv("PORT", default=5000))
